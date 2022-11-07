199dd3c175664747eda14beee3b4454f98318121
donaldyang <Donald_Yang17@hotmail.com>
2022-11-07 16:48:57 -0500

    added details function

--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,18 @@
 from django.shortcuts import render
+from .models import Game
 
-from django.http import HttpResponse
-
-# Models
-
-class Game:
-    def __init__ (self, name, genre, description, publisher, release_year):
-        self.name = name
-        self.genre = genre
-        self.description = description
-        self.publisher = publisher
-        self.release_year = release_year
-
-games = [
-    Game('League of Legends', 'MOBA', "Most popular game in the world.", "Riot Games", 2009),
-    Game('Starcraft', 'RTS', "The first Esports game ever.", "Blizzard Entertainment", 1998),
-    Game('Call of Duty: Modern Warfare 2', 'FPS',"The best game in the franchise.", "Activision", 2009),
-]
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request,'about.html')
 
 def about(request):
     return render(request,'about.html')
 
 def games_index(request):
+    games = Game.objects.all()
     return render(request,'games/index.html', { 'games':games})
 
+def games_detail(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request,'games/detail.html', {'game':game})
